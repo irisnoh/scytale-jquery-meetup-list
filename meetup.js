ce94ee70b3533e214935e59f84f8e18b77c9aa60
Iris Noh <irisnoh22@gmail.com>
2019-10-04 12:11:49 -0500

    console log group date and group meet up date

--- meetup.js
+++ meetup.js
@@ -1,17 +1,23 @@
 console.log('in javascript');
+let meetupCounter = 3;
 
 $(document).ready(onReady);
-
-let totalMeetups = 10;
-
 function onReady() {
+    $('#addButton').on('click', handleAddGroupClick)
+}
+
+function handleAddGroupClick() {
+    console.log('new group is being added');
+    console.log($('#newGroupId').val());
+    console.log($('#newMeetupDateId').val());
+}
+
+
+/*function onReady() {
     console.log('I guess Im ready now');
     $('#addButton').on('click', function () {
         console.log('I was clicked');
         let totalMeetups = $('#meetupCounter').text();
         console.log(totalMeetups);
         totalMeetups++;
-        $('#meetupCounter').text(totalMeetups);
-    })
-
-}
+        $('#meetupCounter').text(totalMeetups);*/
